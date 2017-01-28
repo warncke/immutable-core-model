fd7e3b0e0bc6435956f3a9ddd03d00ca122476c3
Ersun Warncke <ersun.warncke@gmail.com>
2017-01-28 12:58:26 -0400

    add looksLike function to ImmutableCoreModel class

---
+++ test/looks-like.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const ImmutableCoreModel = require('../lib/immutable-core-model')
+const chai = require('chai')
+const immutable = require('immutable-core')
+
+const assert = chai.assert
+
+describe('immutable-core-model - looksLike', function () {
+
+    before(function () {
+        // reset global module namespace
+        immutable.reset()
+    })
+
+    it('should expose looksLike method', function () {
+        assert.isFunction(ImmutableCoreModel)
+    })
+
+
+    it('should return false if object does not look like', function () {
+        assert.isFalse(ImmutableCoreModel.looksLike({}))
+    })
+
+    it('should return true if object does look like', function () {
+        // create new model
+        var fooModel = new ImmutableCoreModel({name: 'foo'})
+        // model should look like an ImmutableCoreModel
+        assert.isTrue(ImmutableCoreModel.looksLike(fooModel))
+    })
+
+}) 
